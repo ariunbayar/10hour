553e99a2abfada1ed32f496b70ca79a658642e14
Ariunbayar Baterdene <ariunbayar.b@gmail.com>
2014-07-25 15:43:07 +0800

    sqlite basic demo

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,34 @@
-import time
+import os
+import sqlite3
+
+
+def db_connect():
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    conn = sqlite3.connect(cur_dir + '/10hour.db')
+    return conn
+
+
+def db_migrate(cursor):
+    """ Import database schema """
+    cursor.execute('DROP TABLE user;')
+    cursor.execute('''
+        CREATE TABLE user (
+            id      INTEGER PRIMARY KEY,
+            name    TEXT
+        )
+    ''')
+
+def db_demo(conn):
+    conn = db_connect()
+    with conn:
+        cursor = conn.cursor()
+        # db_migrate(cursor)
+        cursor.execute("INSERT INTO user (name) VALUES ('Paul')")
+        # last_id = cursor.lastrowid  # Get last insert id
+        # data = cursor.fetchone()
+        cursor.execute("SELECT * FROM user")
+        for id, name in cursor.fetchall():
+            print "id: %s, name: %s" % (id, name)
 
 
 def application(env, start_response):
@@ -15,3 +45,4 @@ def application(env, start_response):
     </html>
     """
     return [content]
+
