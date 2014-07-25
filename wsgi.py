fa281942e51cc9a30f5a42ad4e959aa7693862c2
atu <atu.wow@gmail.com>
2014-07-25 15:29:13 +0800

    n/a: redis added

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,32 @@
 import time
+import redis
 
+
+def cacheSet(key, value):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r.set(key, value)
+
+def cacheGet(name):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    return r.get(name)
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    content = """
+    contentBegin = """
     <!DOCTYPE HTML>
     <html>
     <head>
         <link href='/static/favicon.png' rel='shortcut icon' />
     </head>
     <body>
-        Hello World!
+    """
+
+    cacheSet('hello', 'hello world!')
+    contentBody = cacheGet('hello')
+
+    contentEnd = """
     </body>
     </html>
     """
+    content = contentBegin + contentBody + contentEnd
     return [content]
