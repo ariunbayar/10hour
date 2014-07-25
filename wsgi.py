5d07018cc60b7b03454fb845fc4578e30ac80224
Ariunbayar Baterdene <ariunbayar.b@gmail.com>
2014-07-25 20:04:22 +0800

    alpha version

--- wsgi.py
+++ wsgi.py
@@ -26,12 +26,11 @@ def render_homepage(session_id):
             <h1>10hour</h1>
         </div>
         <div class="content">
-        <div id="myElement">Loading the player...</div>
-        <ul id="notify">
-		<li>Started</li>
-	</ul>
+        <div class="video">
+            <div id="main_video" class="video">Loading the player...</div>
+        </div>
         <script type="text/javascript">
-            jwplayer("myElement").setup({
+            jwplayer("main_video").setup({
                 file:  "/media/whatislove.flv",
                 image: "/media/what_is_love_cover.png",
                 autostart: true,
@@ -41,54 +40,203 @@ def render_homepage(session_id):
                 height: 360
             });
 
-			function notify(message) {
-				$('#notify').prepend(li = $('<li></li>'));
-				li.html(message);
-			}
+            var colors = [
+            'AliceBlue',
+            'AntiqueWhite',
+            'Aqua',
+            'Aquamarine',
+            'Azure',
+            'Beige',
+            'Bisque',
+            'Black',
+            'BlanchedAlmond',
+            'Blue',
+            'BlueViolet',
+            'Brown',
+            'BurlyWood',
+            'CadetBlue',
+            'Chartreuse',
+            'Chocolate',
+            'Coral',
+            'CornflowerBlue',
+            'Cornsilk',
+            'Crimson',
+            'Cyan',
+            'DarkBlue',
+            'DarkCyan',
+            'DarkGoldenRod',
+            'DarkGray',
+            'DarkGreen',
+            'DarkKhaki',
+            'DarkMagenta',
+            'DarkOliveGreen',
+            'DarkOrange',
+            'DarkOrchid',
+            'DarkRed',
+            'DarkSalmon',
+            'DarkSeaGreen',
+            'DarkSlateBlue',
+            'DarkSlateGray',
+            'DarkTurquoise',
+            'DarkViolet',
+            'DeepPink',
+            'DeepSkyBlue',
+            'DimGray',
+            'DodgerBlue',
+            'FireBrick',
+            'FloralWhite',
+            'ForestGreen',
+            'Fuchsia',
+            'Gainsboro',
+            'GhostWhite',
+            'Gold',
+            'GoldenRod',
+            'Gray',
+            'Green',
+            'GreenYellow',
+            'HoneyDew',
+            'HotPink',
+            'IndianRed',
+            'Indigo',
+            'Ivory',
+            'Khaki',
+            'Lavender',
+            'LavenderBlush',
+            'LawnGreen',
+            'LemonChiffon',
+            'LightBlue',
+            'LightCoral',
+            'LightCyan',
+            'LightGoldenRodYellow',
+            'LightGray',
+            'LightGreen',
+            'LightPink',
+            'LightSalmon',
+            'LightSeaGreen',
+            'LightSkyBlue',
+            'LightSlateGray',
+            'LightSteelBlue',
+            'LightYellow',
+            'Lime',
+            'LimeGreen',
+            'Linen',
+            'Magenta',
+            'Maroon',
+            'MediumAquaMarine',
+            'MediumBlue',
+            'MediumOrchid',
+            'MediumPurple',
+            'MediumSeaGreen',
+            'MediumSlateBlue',
+            'MediumSpringGreen',
+            'MediumTurquoise',
+            'MediumVioletRed',
+            'MidnightBlue',
+            'MintCream',
+            'MistyRose',
+            'Moccasin',
+            'NavajoWhite',
+            'Navy',
+            'OldLace',
+            'Olive',
+            'OliveDrab',
+            'Orange',
+            'OrangeRed',
+            'Orchid',
+            'PaleGoldenRod',
+            'PaleGreen',
+            'PaleTurquoise',
+            'PaleVioletRed',
+            'PapayaWhip',
+            'PeachPuff',
+            'Peru',
+            'Pink',
+            'Plum',
+            'PowderBlue',
+            'Purple',
+            'Red',
+            'RosyBrown',
+            'RoyalBlue',
+            'SaddleBrown',
+            'Salmon',
+            'SandyBrown',
+            'SeaGreen',
+            'SeaShell',
+            'Sienna',
+            'Silver',
+            'SkyBlue',
+            'SlateBlue',
+            'SlateGray',
+            'Snow',
+            'SpringGreen',
+            'SteelBlue',
+            'Tan',
+            'Teal',
+            'Thistle',
+            'Tomato',
+            'Turquoise',
+            'Violet',
+            'Wheat',
+            'White',
+            'WhiteSmoke',
+            'Yellow',
+            'YellowGreen',
+            ];
 
-			function im_start() {
-				$.get('/start', {}, function (data){
-					if (data == 'OK') {
-					   setTimeout(first_blood, 5000);
-					} else {
-					   setTimeout(im_start, 500);
-					}
-				});
-			}
+            window.color_idx = 0;
 
-			function im_finish() {
-				$.get('/finish', {}, function (data){
-					if (data == '1') {
-					   notify('finish');
-					} else {
-					   setTimeout(im_finish, 1000);
-					}
-				});
-			}
+            function notify(message, time) {
+                var li = $('<li>');
+                $('.notification').prepend(li);
+                var el_msg = $('<div>');
+                var el_time = $('<div>');
+                li.append(el_msg).append(el_time);
+                el_msg.html(message);
+                el_time.html(time);
+                window.color_idx = ~~(Math.random() * colors.length)
+                el_msg.css('color', colors[window.color_idx]);
+                //window.color_idx++;
+            }
 
-			function first_blood() {
-				notify('FIRST BLOOD!');
-				setTimeout(double_kill, 5000);
-			}
+            function im_start() {
+                $.get('/start', {}, function (data){
+                    if (data == 'OK') {
+                       setTimeout(first_blood, 5000);
+                    } else {
+                       setTimeout(im_start, 500);
+                    }
+                });
+            }
 
-			function double_kill() {
-				notify('DOUBLE KILL!');
-				setTimeout(triple_kill, 5000);
-			}
+            function im_finish() {
+                $.get('/finish', {}, function (data){
+                    if (data == '1') {
+                       notify('Finished', '');
+                    } else {
+                       setTimeout(im_finish, 1000);
+                    }
+                });
+            }
 
-			function triple_kill() {
-				notify('TRIPLE KILL!');
-				im_finish();
-			}
-			im_start();
+            function first_blood() {
+                notify('FIRST BLOOD!', '5 sec');
+                setTimeout(double_kill, 5000);
+            }
+
+            function double_kill() {
+                notify('DOUBLE KILL!', '10 sec');
+                setTimeout(triple_kill, 5000);
+            }
+
+            function triple_kill() {
+                notify('TRIPLE KILL!', '15 sec');
+                im_finish();
+            }
+            im_start();
         </script>
 
         <ul class="notification">
-            <li><div>First blood</div><div>5min</div></li>
-            <li><div>First blood</div><div>5min</div></li>
-            <li><div>First blood</div><div>5min</div></li>
-            <li><div>First blood</div><div>5min</div></li>
-            <li><div>First blood</div><div>5min</div></li>
+            <li><div>Started</div><div></div></li>
         </ul>
         </div>
         <div class="footer">&copy; 2014</div>
