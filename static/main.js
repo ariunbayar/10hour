c21611774a67dee612d500d4161bd492f273636b
Ariunbayar Baterdene <ariunbayar.b@gmail.com>
2014-07-28 15:23:41 +0800

    Timed messages

--- static/main.js
+++ static/main.js
@@ -1,5 +1,4 @@
-jwplayer.key="BSxpAaTPudTB38Uc3YCYtneTFkEHaq90o/XEUw==";
-var player = jwplayer("main_video").setup({
+var player_options = {
     file:  "/media/what_is_love.flv",
     image: "/media/what_is_love_cover.jpg",
     //autostart: true,
@@ -8,170 +7,19 @@ var player = jwplayer("main_video").setup({
     controls: false,
     width: 640,
     height: 360
-});
-
-var colors = [
-    'AliceBlue',
-    'AntiqueWhite',
-    'Aqua',
-    'Aquamarine',
-    'Azure',
-    'Beige',
-    'Bisque',
-    'Black',
-    'BlanchedAlmond',
-    'Blue',
-    'BlueViolet',
-    'Brown',
-    'BurlyWood',
-    'CadetBlue',
-    'Chartreuse',
-    'Chocolate',
-    'Coral',
-    'CornflowerBlue',
-    'Cornsilk',
-    'Crimson',
-    'Cyan',
-    'DarkBlue',
-    'DarkCyan',
-    'DarkGoldenRod',
-    'DarkGray',
-    'DarkGreen',
-    'DarkKhaki',
-    'DarkMagenta',
-    'DarkOliveGreen',
-    'DarkOrange',
-    'DarkOrchid',
-    'DarkRed',
-    'DarkSalmon',
-    'DarkSeaGreen',
-    'DarkSlateBlue',
-    'DarkSlateGray',
-    'DarkTurquoise',
-    'DarkViolet',
-    'DeepPink',
-    'DeepSkyBlue',
-    'DimGray',
-    'DodgerBlue',
-    'FireBrick',
-    'FloralWhite',
-    'ForestGreen',
-    'Fuchsia',
-    'Gainsboro',
-    'GhostWhite',
-    'Gold',
-    'GoldenRod',
-    'Gray',
-    'Green',
-    'GreenYellow',
-    'HoneyDew',
-    'HotPink',
-    'IndianRed',
-    'Indigo',
-    'Ivory',
-    'Khaki',
-    'Lavender',
-    'LavenderBlush',
-    'LawnGreen',
-    'LemonChiffon',
-    'LightBlue',
-    'LightCoral',
-    'LightCyan',
-    'LightGoldenRodYellow',
-    'LightGray',
-    'LightGreen',
-    'LightPink',
-    'LightSalmon',
-    'LightSeaGreen',
-    'LightSkyBlue',
-    'LightSlateGray',
-    'LightSteelBlue',
-    'LightYellow',
-    'Lime',
-    'LimeGreen',
-    'Linen',
-    'Magenta',
-    'Maroon',
-    'MediumAquaMarine',
-    'MediumBlue',
-    'MediumOrchid',
-    'MediumPurple',
-    'MediumSeaGreen',
-    'MediumSlateBlue',
-    'MediumSpringGreen',
-    'MediumTurquoise',
-    'MediumVioletRed',
-    'MidnightBlue',
-    'MintCream',
-    'MistyRose',
-    'Moccasin',
-    'NavajoWhite',
-    'Navy',
-    'OldLace',
-    'Olive',
-    'OliveDrab',
-    'Orange',
-    'OrangeRed',
-    'Orchid',
-    'PaleGoldenRod',
-    'PaleGreen',
-    'PaleTurquoise',
-    'PaleVioletRed',
-    'PapayaWhip',
-    'PeachPuff',
-    'Peru',
-    'Pink',
-    'Plum',
-    'PowderBlue',
-    'Purple',
-    'Red',
-    'RosyBrown',
-    'RoyalBlue',
-    'SaddleBrown',
-    'Salmon',
-    'SandyBrown',
-    'SeaGreen',
-    'SeaShell',
-    'Sienna',
-    'Silver',
-    'SkyBlue',
-    'SlateBlue',
-    'SlateGray',
-    'Snow',
-    'SpringGreen',
-    'SteelBlue',
-    'Tan',
-    'Teal',
-    'Thistle',
-    'Tomato',
-    'Turquoise',
-    'Violet',
-    'Wheat',
-    'White',
-    'WhiteSmoke',
-    'Yellow',
-    'YellowGreen',
-];
-
-window.color_idx = 0;
-
-function notify(message, time) {
-    var li = $('<li>');
-    $('.notification').prepend(li);
-    var el_msg = $('<div>');
-    var el_time = $('<div>');
-    li.append(el_msg).append(el_time);
-    el_msg.html(message);
-    el_time.html(time);
-    window.color_idx = ~~(Math.random() * colors.length)
-    el_msg.css('color', colors[window.color_idx]);
-    //window.color_idx++;
-}
+};
 
 function im_start() {
     $.get('/start', {}, function (data){
         if (data == 'OK') {
-            setTimeout(first_blood, 5000);
+            $('.video, .messages').show();
+            $('.landing').hide();
+            animate_by_time('.messages');
+            jwplayer.key="BSxpAaTPudTB38Uc3YCYtneTFkEHaq90o/XEUw==";
+            var player;
+            player = jwplayer("main_video").setup(player_options);
+            player.play();
+            setTimeout(im_finish, 10000);
         } else {
             setTimeout(im_start, 500);
         }
@@ -181,29 +29,40 @@ function im_start() {
 function im_finish() {
     $.get('/finish', {}, function (data){
         if (data == '1') {
-            notify('Finished', '');
+            $('.messages .complete').show();
         } else {
             setTimeout(im_finish, 1000);
         }
     });
 }
 
-function first_blood() {
-    notify('FIRST BLOOD!', '5 sec');
-    setTimeout(double_kill, 5000);
-}
-
-function double_kill() {
-    notify('DOUBLE KILL!', '10 sec');
-    setTimeout(triple_kill, 5000);
-}
-
-function triple_kill() {
-    notify('TRIPLE KILL!', '15 sec');
-    im_finish();
+function animate_by_time(container){
+    var css_before = {
+        'font-size': '0.1em',
+        'opacity': .01
+    }
+    var css_after = {
+        'font-size': '1em',
+        'opacity': 1
+    }
+    $(container + ' [data-animate]').each(function(idx, el){
+        var $el = $(el);
+        var data = $el.attr('data-animate').split(';');
+        var animate_at = parseFloat(data[0]) * 1000;
+        var animate_duration = parseFloat(data[1]) * 1000;
+        var hide_at = parseFloat(data[2]) * 1000;
+        var hide_fn = function(){ $el.hide(); }
+        var animate_fn = function(){
+            $el.show()
+                .css(css_before)
+                .animate(css_after, animate_duration);
+        };
+        $el.hide();
+        setTimeout(animate_fn, animate_at);
+        if (hide_at) setTimeout(hide_fn, hide_at);
+    });
 }
 
 $(function(){
-    player.play();
-    im_start();
+    animate_by_time('.landing');
 });
